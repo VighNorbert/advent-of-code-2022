03701fc0d42b375691a684b9d5960c040ebb1d65
Norbert Vígh <norbertusvigh@gmail.com>
2022-12-09 15:17:17 +0100

    empty scripts + readme update

--- src/empty.ts
+++ src/empty.ts
@@ -1,5 +1,11 @@
-export const function1 = (s: string) => {};
+const parse = (s: string) => s.trim().split('\n');
+
+export const function1 = (s: string) => {
+	return 0;
+};
 exports.first = function1;
 
-export const function2 = (s: string) => {};
+export const function2 = (s: string) => {
+	return 0;
+};
 exports.second = function2;
